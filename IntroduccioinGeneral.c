25b1eadcb10e30158938ae876959e298bc70fdf1
Julissa <jn.valenzuela17@info.uas.edu.mx>
2021-08-15 14:10:03 -0600

    1.3 La proposicion for

--- IntroduccioinGeneral.c
+++ IntroduccioinGeneral.c
@@ -4,36 +4,10 @@ una tabla de temperaturas Fahrenheit y sus equivalentes centígrados o
 Celsius:*/
 
 main(){
-    int F, C;
-    int low, up, step;
+    int F;
 
-    /*low=0; //Limite inferior de la tabla
-    up=300; //Limite superior 
-    step= 20; //tamaño del incremento
-
-    F=low;
-    printf("Fahrenheit\tCelsius \n");
-    while (F<=up){
-        C= 5*(F-32)/9;
-        printf("%3d\t%11d\n", F, C);
-        F= F+ step;
-    }*/
-
-    low=0; //Limite inferior de la tabla
-    up=160; //Limite superior 
-    step= 20; //tamaño del incremento
-
-    C=low;
-    printf("Celsius\tFahrenheit \n");
-    while (C<=up){
-        F=((C/5)*9)+32;
-        printf("%3d\t%7d\n", C, F);
-        C= C+ step;
+    for(F=300; F>=0; F=F-20){
+        printf("%3d%6.1f\n", F, (5.0/9.0)*(F-32));
     }
-
-    /*Se multiplica por 5 y después se divide entre 9, en vez de multiplicar por 5/9 
-    porque C, la división de enteros trunca el resultado: cualquier parte fraccionaria se descarta. 
-    Puesto que 5 y 9 son enteros, 5/9 sería truncado a cero y así todas las temperaturas Celsius
-    se reportarían como cero*/
 
 }
