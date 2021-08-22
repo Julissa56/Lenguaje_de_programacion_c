058a663609568b458d44f8e4112b9ad86e80c286
Julissa <jn.valenzuela17@info.uas.edu.mx>
2021-08-22 15:06:23 -0600

    1.5.4 Conteo de palabras

--- IntroduccioinGeneral.c
+++ IntroduccioinGeneral.c
@@ -1,75 +1,43 @@
-/*El siguiente programa cuenta espacios en blanco, tabuladores, nuevas líneas a la entrada. 
-   
-    Escriba un programa que copie su entrada a la salida, reemplazando cada cadena de uno 
-        o más blancos por un solo blanco.
-    
-    Escriba un programa que copie su entrada a la salida, reemplazando cada tabulación por \t, 
-        cada retroceso por \b y cada diagonal invertida por \\. Esto hace que las tabulaciones y 
-        los espacios sean visibles sin confusiones. □*/
+/*El siguiente programa cuenta las líneas, 
+palabras y caracteres  usando la definición de que 
+una palabra es cualquier secuencia de caracteres que no 
+contiene espacio en blanco ni tabulación ni nueva línea*/
   
 
 #include <stdio.h>
-#include <stdlib.h>
+
+#define IN	1	//dentro de una palabra
+#define OUT 0	//fuera de una palabra
 
 void main()
 {
-	int c, e_blanco=0, tab=0, n_linea=0, flag_b=1;
-	int borrado_b=0;
+	int c, numCaracter, numPalabra, numLinea, state;
+
+	state =OUT;
+	numCaracter = numLinea = numPalabra =0;
 
 	while((c=getchar())!=EOF){
-
-		switch(c){
-			case ' ': 
-				++e_blanco; 
-				if(flag_b==1){
-					printf("%c", c);
-				}
-				flag_b=0;
-			break;
-
-			case '\b':
-				printf("%ct", 92);
-				flag_b=1;
-			break;
-
-			case 92:
-				printf("%c%c", 92, 92);
-				flag_b=1;
-			break;
-
-			case '\t': 
-				++tab; 
-				printf("%ct", 92);
-				flag_b=1;
-			break;
-
-			case '\n': 
-				++n_linea;
-				printf("%c", c);
-				flag_b=1; 
-			break;
-
-			default: 
-				printf("%c", c);
-				flag_b=1; 
-			break;
-		} 
+		++numCaracter;
+		if(c=='\n'){
+			++numLinea;
+		}
+		if(c==' ' || c=='\n' || c=='\t'){
+			state= OUT;
+		}
+		else if(state==OUT){
+			state=IN;
+			++numPalabra;	
+		} 	 
 	}
-	printf("Espacios en blanco en texto original: %d\n", e_blanco- borrado_b);
-	printf("Tabuladores: %d\n", tab);
-	printf("Renglones: %d\n", n_linea);
+	printf("Lineas: %d	Palabras:%d  Caracteres:%d\n", numLinea, numPalabra, numCaracter);
+	
 }
 
-//printf("Numero de lineas: %d\n", n_linea);   
-//printf("Numero de tabulaciones: %d\n", tab); 
-//printf("Numero de blancos: %d\n", blanco);  
 /*
---Un carácter escrito entre apóstrofos representa un valor entero igual al valor
-    numérico del carácter en el conjunto de caracteres de la máquina
--- por ejemplo 'A' es una constante de carácter  
---Las secuencias de escape que se utilizan en constantes de cadena también 
-    son legales en constantes de carácter
---Contar líneas es solamente contar caracteres nueva línea
+-Las constantes simbólicas IN y OUT hacen el programa más legible
+-Las asignaciones se asocian de derecha a izquierda
+- El operador ¡¡ significa “O”
+-La precedencia del AND es mass grande que la del OR
+-Las expresiones con AND o OR se evaluan de izquierda a derecha
 */
-
 
