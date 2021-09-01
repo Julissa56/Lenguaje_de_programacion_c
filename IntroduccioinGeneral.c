ac4558aaea38b5bd6f4ebeeeb9c31f54f07e37bc
Julissa <jn.valenzuela17@info.uas.edu.mx>
2021-08-31 20:04:31 -0600

    1.9 Arreglo de caracteres/ Ejercicio 1-17

--- IntroduccioinGeneral.c
+++ IntroduccioinGeneral.c
@@ -1,30 +1,50 @@
 /*
-	 Escriba un programa que lea un conjunto de líneas de texto e imprima la de mayor longitud
+	Ejercicio 1-17. Escriba un programa que imprima todas las líneas de entrada que
+	sean mayores de X caracteres. 
 */
   
 
 #include <stdio.h>
 #define MAXLINE		1000						//Tamaño máximo de la linea de entrada
+#define MIN			10							//Caracteres minimos para imprimir
+#define FLINE		1							//Primera linea que cumple con el minimo de caracteres
+#define MLINE		0							//Linea que cumple el minimo caracteres (no es la primera)
+
+
 
 
 int obtener_linea(char line[], int maxline);			//Funciones prototipo
-void copiar(char hacia[], char desde[]);	
+void copiar(char hacia[], char desde[]);
+void agregar(char anterior[], char nueva[]);
+void ImprimirLineasVariables(int conteo[], int limite);
+
 
 void main()
 {
-	int longitud, max_lon=0;
+	int longitud, max_lon=0, state=FLINE, i=0;
 	char line[MAXLINE];
-	char longest[MAXLINE];
+	char mayor[MAXLINE];
+	int  conteoMayores80[MAXLINE];
 
-	while((longitud=obtener_linea(line, MAXLINE))>0){		//main y getline se comunican a traves de un par de argumentos y un valor de retorno
-		if(longitud > max_lon){
+	while((longitud=obtener_linea(line, MAXLINE))>0){		//Obtiene la longitud de la linea
+		
+		if((longitud> MIN) && (state ==MLINE)){				//Si no es la primer linea con el minimo de caracteres
+			conteoMayores80[i]=longitud;				//Posible reemplazo con funcion "Registro_Longitudes"
+			++i;
+			agregar(mayor, line);
+		}
+		if((longitud > MIN) && (state==FLINE)){				//Si es la primer linea con el minimno de caracteres
 			max_lon=longitud;
-			copiar(longest, line);
+			conteoMayores80[i]=longitud;
+			++i;
+			copiar(mayor, line);
+			state=MLINE;
 		}
 	}
 
 	if(max_lon>0){	//Hubo una linea							
-		printf("%s", longest);
+		printf("%s", mayor);
+		
 	}
 	
 }
@@ -49,6 +69,21 @@ void copiar(char to[], char from[]){					//Copia 'from' en 'to'
 
 	while((to[i]=from[i]) != '\0'){
 		++i;
+	}
+}
+
+void agregar(char anterior[], char nueva[]){
+	int i=0;
+	int j=0;
+	for(i=0; anterior[i]!='\0'; ++i){					//Encuentra donde acabo la ultima cadena
+		;
+	}
+
+	//anterior[i]='\n';									//Reemplaza el fin de string por un salto de linea
+	//++i;
+    while((anterior[i]=nueva[j])!='\0'){					//Sigue escribiendo donde se quedó, hasta que encuentra el caracter fin de string
+		++i;
+		++j;
 	}
 }
 
